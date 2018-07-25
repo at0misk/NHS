798ace44dbf4d2c01b6b071fee1120478f3c3d65
at0misk <ktp925@gmail.com>
2018-07-25 10:43:56 -0700

    conf

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://172.31.21.163/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
