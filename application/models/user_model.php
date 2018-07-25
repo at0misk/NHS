493ad726c2e2a87091aabc582a6719b31462fd86
at0misk <ktp925@gmail.com>
2018-07-25 12:01:14 -0700

    conf

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -2,7 +2,7 @@
     class User_model extends CI_Model {
          function register($data)
          {
-            $query = "INSERT INTO asdfusers (first, last, email, password, created_at, updated_at) VALUES (?,?,?,?, NOW(), NOW())";
+            $query = "INSERT INTO users (first, last, email, password, created_at, updated_at) VALUES (?,?,?,?, NOW(), NOW())";
             $values = array($data['first'], $data['last'], $data['email'], $data['password']);
             $this->db->query($query, $values);
             $user_info = array(
@@ -14,7 +14,7 @@
          }
          function get_user_by_email($email)
          {
-            $query = "SELECT * FROM asdfusers WHERE email = ?";
+            $query = "SELECT * FROM users WHERE email = ?";
             $values = array($email);
             return $this->db->query($query, $values)->row_array();
          }
